aecf36f61f437997224641ad8d1f1795f0838090
manakong <128371916+manakong@users.noreply.github.com>
2025-03-17 17:06:09 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBRarN_Rj0EBhVQXj92w2x3wVuORcNXBuA",
+    authDomain: "guess-number-game-a223f.firebaseapp.com",
+    projectId: "guess-number-game-a223f",
+    storageBucket: "guess-number-game-a223f.firebasestorage.app",
+    messagingSenderId: "996802770979",
+    appId: "1:996802770979:web:df1a401b5d92c85cdd1996"
+  };
